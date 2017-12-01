2036f766dd4cdbb2873c8bac20f801d399a58cc3
Administrator <Administrator@PC264>
2017-12-01 07:35:25 +0700

    Add project files.

---
+++ ConsoleAppLab1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
